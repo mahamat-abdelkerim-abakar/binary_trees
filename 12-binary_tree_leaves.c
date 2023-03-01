3219777f8e67336b1d9d0c5700e6250041bac728
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2023-03-01 19:39:58 +0530

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a tree
- *@tree: pointer to the tree
- * Return: number of leaves
+ * binary_tree_leaves - Counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: If the tree is NULL, must return 0
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  if (tree->left == NULL && tree->right == NULL)
-    return (1);
-  else
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	size_t leaves = 0;
+
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
 }
